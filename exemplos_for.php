eb04caf3896f7104a9fe61a8d07629d2e0617ec3
JOAO RAFAEL DE CARVALHO BISSOLOTI <joao.1978@aluno.pr.senac.br>
2023-04-03 07:54:04 -0300

    Exemplo FOR

---
+++ exemplos_for.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Exemplos For - PHP</title>
+    </head>
+    <body>
+
+        <?php
+        //Laço de repetição FOR em PHP_ROUND_HALF_DOWN
+
+        //Imprima os valores de 1 ao 10 em sua página WEB
+
+        echo "1";
+        echo "</br>";
+        echo "2";
+        echo "</br>";
+        echo "3";
+        echo "</br>";
+        echo "4";
+        echo "</br>";
+        echo "5";
+        echo "</br>";
+        echo "6";
+        echo "</br>";
+        echo "7";
+        echo "</br>";
+        echo "8";
+        echo "</br>";
+        echo "9";
+        echo "</br>";
+        echo "10";
+        echo "</br>";
+
+        for($numero=0; $numero<=10; $numero++){
+            echo $numero . "<br>";
+        }
+
+        ?>
+
+    </body>
+</html>
