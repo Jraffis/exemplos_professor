14e349731f5fbf9ff449fa4a9b14a1460bbc105a
JOAO RAFAEL DE CARVALHO BISSOLOTI <joao.1978@aluno.pr.senac.br>
2023-04-14 07:41:16 -0300

    Exemplo IF

---
+++ exemplo_if.php
@@ -0,0 +1,14 @@
+<?php
+    #Exemplo de código com IF
+
+    $idade = 30;
+
+
+    if ($idade <= 17) {
+        echo("Sua idade é menor de 18 anos");
+    }elseif ($idade >18) {
+        echo("Sua idade é maior de 18 anos");
+    }else{
+        echo("Não corresponde a uma idade");
+    }
+?>
